8e7797286f8f83c5809cff92e3af262e813d2d4d
yyamamoto-yy <yamayu0214@yahoo.co.jp>
2020-04-30 17:41:17 +0900

    プロジェクト ファイルを追加します。

---
+++ WebApp/Controllers/SampleController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebApp.Controllers
+{
+    public class SampleController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            var model = new WebApp.Models.SampleModel();
+            model.Text = "Hello, World!";
+            return View(model);
+        }
+
+        public IActionResult GetGrid(WebApp.Models.SampleModel model)
+        {
+            model.Text = "こんにちは、世界！";
+            return PartialView("Grid", model);
+        }
+              
+    }
+}
