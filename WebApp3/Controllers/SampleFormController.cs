8339ff5b12f952db3b886cbf402563bc161f8879
yyamamoto-yy <yamayu0214@yahoo.co.jp>
2020-05-01 17:41:02 +0900

    2020_0501_17:40

---
+++ WebApp3/Controllers/SampleFormController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp3.Controllers
+{
+    public class SampleFormController : Controller
+    {
+        public IActionResult Index()
+        {
+            var form = new WebApp3.Models.SampleFormModel();
+            return View(form); //View("Index", form)と同じ
+        }
+
+        public IActionResult Submit(WebApp3.Models.SampleFormModel form)
+        {
+            return View("Index", form);
+        }
+    }
+}
---
+++ WebApp3/Models/SampleFormModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApp3.Models
+{
+    public class SampleFormModel
+    {
+        public string 不動産番号 { get; set; }
+        public string 所在 { get; set; }
+        public bool 物件種別_土地 { get; set; }
+        public bool 物件種別_建物 { get; set; }
+        public bool 物件種別_専有 { get; set; }
+        public bool 所有権名義人単位で検索する { get; set; }
+        public string 所有権名義人氏名 { get; set; }
+    }
+}
