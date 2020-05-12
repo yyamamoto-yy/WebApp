e38d1d36d6f56b4f045cec63660644b3dd02d9b3
yyamamoto-yy <yamayu0214@yahoo.co.jp>
2020-05-12 10:36:41 +0900

    test追加

---
+++ test/Controllers/SampleFormController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace test.Controllers
+{
+    public class SampleFormController : Controller
+    {
+        public IActionResult Index()
+        {
+            var form = new test.Models.SampleFormModel();
+            form.Text = "0";
+            return View(form); //View("Index", form)と同じ
+        }
+
+        public IActionResult Submit(test.Models.SampleFormModel form)
+        {
+            form.Text = "12345";
+
+            return View("Index", form);
+        }
+    }
+}
---
+++ test/ViewComponent/SampleViewComponent.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace test.ViewComponents
+{
+    public class SampleViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke(test.Models.SampleFormModel model)
+        {
+            return View(model);
+        }
+    }
+}
